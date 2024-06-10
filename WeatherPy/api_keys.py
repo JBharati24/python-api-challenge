b9f5c9585e46d246035de650a34d8c12e253d817
“JBharati24” <janhavibharati@macbook-pro.home>
2024-06-10 10:06:02 -0400

    Initial commit with starter files

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "6e9af3cebccf02221fff26f440db809f"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "ee932dd0414641b8ab9ba56f043e51fb"
